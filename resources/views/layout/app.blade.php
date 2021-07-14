1315bd72b4a22cb5fb79fa7e8909e07cf112165e
jynnepo <jbernardonepo@gmail.com>
2021-07-14 15:04:10 +0800

    modal

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -338,6 +338,31 @@
                 </div>
             </div>
         </footer>
+        
+        @foreach ($services as $data)
+        <!-- Portfolio Modals-->
+        <!-- Portfolio item 1 modal popup-->
+        <div class="portfolio-modal modal fade" id="{{ str_replace(' ', '-', strtolower($data->name)) . '-' . $data->id }}" tabindex="-1" role="dialog" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="close-modal" data-bs-dismiss="modal"><img src="assets/img/close-icon.svg" alt="Close modal" /></div>
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col-lg-8">
+                                <div class="modal-body">
+                                    <!-- Project details-->
+                                    <h2 class="text-uppercase">{{ $data->name }}</h2>
+                                    
+                                    <p class="item-intro text-muted">{{ 'Name: ' . $data->user->name  }}</p>
+                                    <p class="item-intro text-muted">{{ 'Location: ' . $data->location }}</p>
+                                    <img class="img-fluid d-block mx-auto" src="assets/img/portfolio/garden landscape.jpg" alt="..." />
+                                    {{-- <img class="img-fluid d-block mx-auto" src="assets/img/portfolio/1.jpg" alt="..." /> <br>
+                                    <img class="img-fluid d-block mx-auto" src="assets/img/portfolio/1.jpg" alt="..." /> <br> --}}
+                                    <p>{{ $data->description }}</p>
+                                    <button class="btn btn-primary btn-xl text-uppercase" data-bs-dismiss="modal" type="button">
+                                        <i class="fas fa-times me-1"></i>
+                                        RESERVE
+                                    </button> 
                <!-- Portfolio Modals-->
         <!-- Portfolio item 1 modal popup-->
         <div class="portfolio-modal modal fade" id="portfolioModal1" tabindex="-1" role="dialog" aria-hidden="true">
